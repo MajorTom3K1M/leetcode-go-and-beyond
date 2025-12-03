0f4fd4ef79b8dac89f736c1c19bfab9a8a14c916
MajorTom <invaderzgame@gmail.com>
2025-12-03 17:20:15 +0700

    add: middle of the linked list

---
+++ go/middleOfLinkedList/middleOfLinkedList.go
@@ -0,0 +1,29 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+func main() {
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	head.Next.Next.Next = &ListNode{Val: 4}
+	head.Next.Next.Next.Next = &ListNode{Val: 5}
+
+	middle := middleNode(head)
+	println(middle.Val)
+}
