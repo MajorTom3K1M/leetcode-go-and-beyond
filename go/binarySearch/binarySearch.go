8c082cfcd6b5131ffb4fb426d3c8404c56b0b176
MajorTom <invaderzgame@gmail.com>
2025-12-04 18:58:58 +0700

    add: binary search

---
+++ go/binarySearch/binarySearch.go
@@ -0,0 +1,26 @@
+package main
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	target := 9
+	result := search(nums, target)
+	println("Result:", result)
+}
